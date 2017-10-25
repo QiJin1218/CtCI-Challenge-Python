aaecba95a70a0b0f63c72ed88cfb29e6e25aa73d
Qi Jin <qi_jin1218@hotmail.com>
2017-10-24 22:22:27 -0500

    Binary Tree Initial

--- 4. Trees/4.0.py
+++ 4. Trees/4.0.py
@@ -2,14 +2,57 @@
 
 import sys
 
-#Tree
+#Binary Tree
 
 class Tree:
 
-	def __init__(self):
-		self.items = []
+	def __init__(self,r):
+		self.root = r
+		self.left = None
+		self.right = None
 
-# stack = Stack()
-# print stack.isEmpty()
-# newStack = createStack([1,2,3])
-# printStack(newStack)
+	#Add a value to a tree
+	def addtoTree(self,v):
+
+		t = self.root 
+
+		if(t is None):
+			self.root = v
+		else:
+			if(t > v):
+				if(self.left is None):
+					self.left = Tree(v)
+				else:
+					self.left.addtoTree(v)
+			else:
+				if(self.right is None):
+					self.right = Tree(v)
+				else:
+					self.right.addtoTree(v)
+
+	#Prints out nodes of trees in-order trabersal
+	def showTree(self):
+
+		t = self.root
+
+		if(t is None):
+			return ""
+		else:
+			if(self.left is not None):
+				self.left.showTree()
+			print t
+			if(self.right is not None):
+				self.right.showTree()
+
+def listtoTree(l):
+
+	t = Tree(l[0])
+
+	for i in l[1:]:
+		t.addtoTree(i)
+
+	return t
+
+l = [1,2,5,2,7,3,9,1]
+tree = listtoTree(l)
+tree.showTree()
