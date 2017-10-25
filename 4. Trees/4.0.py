62006cece051d5579b1b06c591d35393377716d7
Qi Jin <qi_jin1218@hotmail.com>
2017-10-24 21:49:16 -0500

    Commit 4. Trees

---
+++ 4. Trees/4.0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+
+#Tree
+
+class Tree:
+
+	def __init__(self):
+		self.items = []
+
+# stack = Stack()
+# print stack.isEmpty()
+# newStack = createStack([1,2,3])
+# printStack(newStack)
